81648b568756b5bd8a9ef808cc93531139e86bf4
Igor Dvorkin <idvorkin@gmail.com>
2018-08-12 15:45:46 -0700

    Process with Black.

--- add-position.py
+++ add-position.py
@@ -3,33 +3,35 @@ from bs4 import BeautifulSoup
 import collections
 
 # TODO: Support the other attributes like size and tags.
-NameAndPosition = collections.namedtuple('NameAndPosition','name position')
+NameAndPosition = collections.namedtuple("NameAndPosition", "name position")
 
 # 1) Read twine->html output, add position to twee output in comments.
 # 2) Read twee+position_comments, load position comments,  re-apply position to twee->html output.
 
 # hardcode for now, later grab from command line
 input_html_file = ".\\airport_builder\\Airport Builder.html"
-twee_file_base=".\\story"
-input_twee_file = f'{twee_file_base}.twee'
-output_file= f'{twee_file_base}.position.twee'
+twee_file_base = ".\\story"
+input_twee_file = f"{twee_file_base}.twee"
+output_file = f"{twee_file_base}.position.twee"
+
 
 def getPositonFromTwineHtmlOutput(twineHtmlOutputFile):
     soup = None
     # not sure why utf8 doesn't work
-    #with open(input_html_file,encoding='utf8') as fp:
-    with open(twineHtmlOutputFile,errors='ignore') as fp:
-        soup = BeautifulSoup(fp, 'html.parser')
+    # with open(input_html_file,encoding='utf8') as fp:
+    with open(twineHtmlOutputFile, errors="ignore") as fp:
+        soup = BeautifulSoup(fp, "html.parser")
 
-    #<tw-passagedata pid="3" name="brian&#39;s buidlers"
+    # <tw-passagedata pid="3" name="brian&#39;s buidlers"
     #   tags="" position="140,548.5999999046326" size="100,100">
 
     # gather all tw-passagedata
-    passages =  soup.find_all('tw-passagedata')
-    data = [ NameAndPosition(p['name'], p['position']) for p in passages]
+    passages = soup.find_all("tw-passagedata")
+    data = [NameAndPosition(p["name"], p["position"]) for p in passages]
     return data
 
-def addPositionCommentsToTweeOutput(nameAndPositions,tweeOutputFile):
+
+def addPositionCommentsToTweeOutput(nameAndPositions, tweeOutputFile):
     lines = open(input_twee_file).readlines()
     output = []
     nameToPos = dict(nameAndPositions)
@@ -43,21 +45,26 @@ def addPositionCommentsToTweeOutput(nameAndPositions,tweeOutputFile):
         name = nameCandidate[1].strip()
         if name in nameToPos:
             # write out the position.
-            positionOutput = f'+ /* POSITION: {nameToPos[name]} */\n' # TBD what's teh story with the \n's
-            print (positionOutput)
+            positionOutput = (
+                f"+ /* POSITION: {nameToPos[name]} */\n"
+            )  # TBD what's teh story with the \n's
+            print(positionOutput)
             output.append(positionOutput)
         else:
-            print (f"ERROR: {name} found in twee file but no position data")
+            print(f"ERROR: {name} found in twee file but no position data")
 
     # open(output_file,"w",lines)
-    open(output_file,"w").writelines(output)
-    
+    open(output_file, "w").writelines(output)
+
     return
+
+
 # Interesting idea - skip the middle input file with twee, do input to output.
 def addPositionDataToTweeHtmlOutput(nameAndPosition):
     nameToPos = dict(nameAndPositions)
-    with open(output_twee_html_file,errors='ignore') as fp:
-        soup = BeautifulSoup(fp, 'html.parser')
+    with open(output_twee_html_file, errors="ignore") as fp:
+        soup = BeautifulSoup(fp, "html.parser")
+
 
 data = getPositonFromTwineHtmlOutput(input_html_file)
-addPositionCommentsToTweeOutput(data,output_file)
+addPositionCommentsToTweeOutput(data, output_file)
